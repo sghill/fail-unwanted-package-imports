b6919858d390a4c40ea85922479ec6ae53778e12
Steve Hill <sghill.dev@gmail.com>
2017-05-13 18:41:07 -0700

    Restructure as Multi-Project

---
+++ acceptance/src/test/java/io/buildwell/acceptance/CalculatorAcceptanceTest.java
@@ -0,0 +1,16 @@
+package io.buildwell.acceptance;
+
+import io.buildwell.foundation.Calculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorAcceptanceTest {
+    private Calculator calc = new Calculator();
+
+    @Test
+    public void shouldAddNegativeNumbers() {
+        int actual = calc.add(-1, 200);
+        assertEquals(199, actual);
+    }
+}
